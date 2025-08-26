f51f287c7c8dbb0ed12d2f5dcc4f7b1362ed827e
Themba14257 <thembanaterichard@gmail.com>
2025-08-26 22:17:36 +0200

    comment out contact form

--- tests/pageObjects/Contact.ts
+++ tests/pageObjects/Contact.ts
@@ -4,12 +4,17 @@ export class ContactPage {
     readonly lastNameInput = '[data-test="last-name"]';
     readonly emailInput = '[data-test="email"]';
     readonly messageTextArea = '[data-test="message"]';
-    readonly sendButton = '[data-test="contact-submit"]';
+    readonly sendButton = 'input[data-test="contact-submit"]';
     readonly subjectDropdownList = '[data-test="subject"]';
     readonly contactNavLink = '[data-test="nav-contact"]';
     readonly contactHeading = 'h3';
     readonly toastMessage = '.Toastify__toast-body';
+    readonly firstNameError = '[data-test="first-name-error"]';
+    readonly lastNameError = '[data-test="last-name-error"]';
+    readonly emailError = 'div[data-test="email-error"]';
+    readonly subjectError = '[data-test="subject-error"]';
     readonly messageError = '[data-test="message-error"]';
+
 
 }
 
